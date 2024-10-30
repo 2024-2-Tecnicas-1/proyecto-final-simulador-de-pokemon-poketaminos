6de678f624179dcbfe9445534e4ac13955851e18
Ximena Capera <ximenacapera79@gmail.com>
2024-10-30 11:36:59 -0500

    Clase Batalla

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -1,5 +1,38 @@
 package simulador.batalla;
 
-public class Batalla {
-    // TODO: Aquí va tu código
+public class Batalla extends Pokemon {
+    
+    Pokemon Charmander = new Pokemon("Charmander", 39, 52, TipoPokemon.FUEGO);
+    Pokemon Squirtle = new Pokemon("Squirtle", 44, 48, TipoPokemon.AGUA);
+    Pokemon Bulbasaur = new Pokemon("Bulbasaur", 45, 49, TipoPokemon.PLANTA);
+    Pokemon Pikachu = new Pokemon("Pikachu",35 , 52, TipoPokemon.ELECTRICO);
+    Pokemon Abra = new Pokemon("Abra", 25, 20, TipoPokemon.PSIQUICO);
+    Pokemon Geodude = new Pokemon("Geodude", 40, 80, TipoPokemon.ROCA);
+    Pokemon Pidgey = new Pokemon("Pidgey", 40, 45, TipoPokemon.VOLADOR);
+    Pokemon Jigglypuff = new Pokemon("Jigglypuff", 115, 45, TipoPokemon.HADA);
+    Pokemon Machop = new Pokemon("Machop", 39, 52, TipoPokemon.LUCHA);
+    Pokemon Grimer = new Pokemon("Grimer", 80, 80, TipoPokemon.VENENO);
+    
+
+  public void iniciarBatalla(Pokemon 1,)        
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
+
 }
